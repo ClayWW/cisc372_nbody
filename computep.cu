c67054ac8a4b4ebf719da11591bfbab7b7473ad2
Clay <clayw@udel.edu>
2023-05-15 18:30:05 -0400

    EVEN FASTER

--- computep.cu
+++ computep.cu
@@ -7,7 +7,16 @@
 
 vector3* values;
 vector3** accels;
-
+/*
+p_compute: performs the same function as compute.c but in parrallel
+Input:
+    vector3* values: 
+    vector3* accels:
+    vector3* d_hPos:
+    vector3* d_hVel:
+    double d_mass:
+Output: None, calculates accelereration matrix and then sums the columns for signle acceleration effect for an object
+*/
 __global__ void p_compute(vector3* values, vector3** accels, vector3* d_hPos, vector3* d_hVel, double *d_mass){
     int currID = blockIdx.x * blockDim.x + threadIdx.x; //current thread block
     int i = currID / NUMENTITIES;
@@ -15,7 +24,7 @@ __global__ void p_compute(vector3* values, vector3** accels, vector3* d_hPos, ve
     int k;
     accels[currID] = &values[currID*NUMENTITIES]; //accels array for all the accel pointers
     if(currID < NUMENTITIES*NUMENTITIES){
-        if(i == j){ //imma keep it a full stack I copied this from the compute.c file
+        if(i == j){ //imma keep it a full stack(no pun intended) I copied this from the compute.c file
             FILL_VECTOR(accels[i][j],0,0,0);
         }else{
             vector3 distance;
@@ -26,7 +35,7 @@ __global__ void p_compute(vector3* values, vector3** accels, vector3* d_hPos, ve
 			FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
         }
         vector3 accel_sum = {(double) *(accels[currID])[0], (double) *(accels[currID])[1], (double) *(accels[currID])[2]}; //sum accelerations
-		/*
+		/* doing this shit manually theres gotta be a better way
         d_hVel[i][0] = d_hVel[i][0]+accel_sum[0]*INTERVAL; //updating the relative acceleration and velocities
 		d_hPos[i][0] = d_hVel[i][0]*INTERVAL;
 		d_hVel[i][1] = d_hVel[i][1]+accel_sum[1]*INTERVAL;
@@ -35,7 +44,7 @@ __global__ void p_compute(vector3* values, vector3** accels, vector3* d_hPos, ve
 		d_hPos[i][2] = d_hVel[i][2]*INTERVAL;
         */
         for(int z = 0; z < 3; z++){ //seeing if for loop is faster
-            d_hVel[i][z] = d_hVel[i][z]+accel_sum[z]*INTERVAL;
+            d_hVel[i][z] +=accel_sum[z]*INTERVAL;
             d_hPos[i][z] = d_hVel[i][z]*INTERVAL;
         }
     }
@@ -43,7 +52,7 @@ __global__ void p_compute(vector3* values, vector3** accels, vector3* d_hPos, ve
 }
 
 
-/*
+/* Error that I literally just cannot figure out for my life
 __global__ void psum(vector3** accels, vector3* accel_sum, vector3* d_hPos, vector3* d_hVel){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if(i < NUMENTITIES){
@@ -73,7 +82,7 @@ void compute(){
     //int threads = ceilf(NUMENTITIES/(float)blocks);
     //dim3 fullgrid(blocks, blocks, 1);
     //dim3 blockdim(threads, threads, 1);
-    cudaMallocManaged((void**) &d_hPos, sizeof(vector3) * NUMENTITIES); //allocating mem for position, velocity, mass, our acceleration and sum functions
+    cudaMallocManaged((void**) &d_hPos, sizeof(vector3) * NUMENTITIES); //allocating mem for position, velocity, mass
     cudaMallocManaged((void**) &d_hVel, sizeof(vector3) * NUMENTITIES);
     cudaMallocManaged((void**) &d_mass, sizeof(double) * NUMENTITIES);
     //cudaMallocManaged((void**) &d_acc, sizeof(vector3) * NUMENTITIES);        
@@ -82,7 +91,7 @@ void compute(){
     cudaMemcpy(d_hVel, hVel, sizeof(vector3) * NUMENTITIES, cudaMemcpyHostToDevice);
     cudaMemcpy(d_mass, mass, sizeof(double) * NUMENTITIES, cudaMemcpyHostToDevice);
     p_compute<<<totalBlocks, blocksize>>>(values, accels, d_hVel, d_hPos, d_mass); //compute accelerations in parallel
-    cudaDeviceSynchronize();
+    cudaDeviceSynchronize(); //make sure its all coming together 
     //psum<<<fullgrid.x, blockdim.x>>>(d_acc, d_sum, d_hPos, d_hVel); //sum in parallel
     //cudaDeviceSynchronize();
     cudaMemcpy(hPos, d_hPos, sizeof(vector3) * NUMENTITIES, cudaMemcpyDeviceToHost); //copy from device to host memory
