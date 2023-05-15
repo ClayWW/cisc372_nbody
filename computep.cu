cc08fd8a3a347c8db76f58d2aa5fdd7da5119d5f
Clay <clayw@udel.edu>
2023-05-15 17:06:09 -0400

    Finished initial compute, changed nbody to cuda

--- computep.cu
+++ computep.cu
@@ -35,8 +35,6 @@ __global__ void psum(vector3 *hVel, vector3* hPos, vector3** accels, vector3* ac
 			for (k=0;k<3;k++)
 				accel_sum[k]+=accels[(i*NUMENTITIES)+j][k];
 		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
 		for (k=0;k<3;k++){
 			hVel[i][k]+=accel_sum[i][k]*INTERVAL;
 			hPos[i][k]=hVel[i][k]*INTERVAL;
@@ -46,4 +44,31 @@ __global__ void psum(vector3 *hVel, vector3* hPos, vector3** accels, vector3* ac
 
 
 void compute(){
+    vector3 *d_hVel;
+    vector3 *d_hPos;
+    vector3 *d_acc;
+    vector3* d_sum;
+    double d_mass;
+    int blocks = ceilf(NUMENTITIES/16.0f);  //defining our blocks and threads
+    int threads = ceilf(NUMENTITIES/(float)blocks);
+    dim3 fullgrid(blocks, blocks, 1);
+    dim3 blockdim(threads, threads, 1);
+    cudaMallocManaged((void**) &d_hPos, sizeof(vector3)*NUMENTITIES); //allocating mem for position, velocity, mass, our acceleration and sum functions
+    cudaMallocManaged((void**) &d_hVel, sizeof(vector3)*NUMENTITIES);
+    cudaMallocManaged((void**) &d_mass, sizeof(double)*NUMENTITIES);
+    cudaMallocManaged((void**) &d_acc, sizeof(vector3)*NUMENTITIES);        
+    cudaMallocManaged((void**) &d_sum, sizeof(vector3)*NUMENTITIES);
+    cudaMemcpy(d_hPos, hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice); //copying data from host to device memory
+    cudaMemcpy(d_hVel, hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mass, mass, sizeof(double)*NUMENTITIES, cudaMemcpyHostToDevice);
+    paccel<<<fullgrid, blockdim>>>(d_hPos,d_acc,d_mass); //compute accelerations in parallel
+    cudaDeviceSynchronization();
+    psum<<<fullgrid.x, blockdim.x>>>(d_acc, d_sum, d_hPos, d_hVel); //sum in parallel
+    cudaMemcpy(hPos, d_hPos, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost); //copy from device to host memory
+    cudaMemcpy(hVel, d_hVel, sizeof(vector3)*NUMENTITIES, cudaMemcpyDeviceToHost);
+    cudaFree(d_hPos); //free everything that was allocated
+    cudaFree(d_hVel);
+    cudaFree(d_mass);
+    cudaFree(d_acc);
+    cudaFree(d_sum);
 }
